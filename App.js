0d845a79f5fc83968f0c82f42278625a260c6f30
Saad Bakshi <isaadbakshi@gmail.com>
2024-11-14 15:31:50 -0800

    02

--- App.js
+++ App.js
@@ -1,113 +1,109 @@
-import React, { useState, useEffect } from 'react'
-import {
-  StyleSheet,
-  Dimensions,
-  View,
-  Text,
-  FlatList,
-  Image,
-  ActivityIndicator,
-} from 'react-native'
-import CalendarStrip from 'react-native-calendar-strip';
+import React, { useState, useEffect } from 'react';
+import {StyleSheet,View,FlatList,Text,Image,TouchableOpacity,ActivityIndicator, Switch,} from 'react-native';
 
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [expandedUserId, setExpandedUserId] = useState(null);
 
-const cardHeight = 250
-const cardTitle = 45
-const cardPadding = 10
-
-const cards = [
-  {
-    name: 'Shot',
-    color: 'red',
-    price: '30 CHF',
-  },
-  {
-    name: 'Juice',
-    color: 'black',
-    price: '64 CHF',
-  },
-  {
-    name: 'Mighty Juice',
-    color: '#BFDBFE',
-    price: '80 CHF',
-  },
-  {
-    name: 'Sandwich',
-    color: '#93C5FD',
-    price: '85 CHF',
-  },
-  {
-    name: 'Combi',
-    color: '#60A5FA',
-    price: '145 CHF',
-  },
-  {
-    name: 'Signature',
-    color: '#3B82F6',
-    price: '92 CHF',
-  },
-  {
-    name: 'Coffee',
-    color: '#2563EB',
-    price: '47 CHF',
-  },
-]
-
-const { height } = Dimensions.get('window')
-
-const App = ({ }) => {
-  const [selected, setSelected] = useState(false)
-  const [space, setSpace] = useState([])
+  const fetchData = async () => {
+    setIsLoading(true);
+    const response = await fetch('https://api.escuelajs.co/api/v1/users?offset=1&limit=100');
+    const rawData = await response.json();
+    setUsers(rawData);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    setSelected(true)
-    const fetchData = async () => {
-      const response = await fetch('https://api.escuelajs.co/api/v1/users?offset=2&limit=100')
-      const rawData = await response.json();
-      setSpace(rawData)
-      setSelected(false)
-    }
-    fetchData()
-    return (() => {
-      setSelected(false)
-    })
-  }, [])
+    fetchData();
+  }, []);
 
+  const toggleAccordion = (userId) => {
+    setExpandedUserId(expandedUserId === userId ? null : userId);
+  };
 
   return (
     <View style={styles.wrapper}>
-    <CalendarStrip
-      scrollable
-      style={{ height: 100, paddingTop: 20, paddingBottom: 10 }}
-      calendarHeaderStyle={{ color: 'black', fontSize: 16 }} 
-      dateNumberStyle={{ color: 'black', fontSize: 14 }}          
-      dateNameStyle={{ color: '#000000', fontSize: 12 }} 
-      highlightDateNameStyle={{ color: '#000', fontSize: 14 }} 
-      highlightDateNumberStyle={{ color: '#000000', fontSize: 18 }}
-      iconContainer={{flex: 0.1}}
-      shouldAllowFontScaling={true}
-      showDate
-      
-    />
-  </View>
-  )
-}
+      {isLoading ?
+        <ActivityIndicator size={'large'} color={'red'} />
+        :
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={users}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View>
+              <TouchableOpacity
+                onPress={() => toggleAccordion(item.id)}
+                style={styles.accordionHeader}>
+                <View style={styles.userInfo}>
+                  <Image
+                    resizeMode='cover'
+                    style={styles.avatar}
+                    source={{ uri: item.avatar }}
+                  />
+                  <Text style={styles.userName}>{item.name}</Text>
+                </View>
+                <Text style={styles.toggleIcon}>{expandedUserId === item.id ? '-' : '+'}</Text>
+              </TouchableOpacity>
+              {expandedUserId === item.id && (
+                <View style={styles.accordionContent}>
+                  <Text style={{ fontSize: 13, fontWeight: '500', color: "#FFFFFF" }} >{`Email: @${item.email}`}</Text>
+                  <Text style={{ fontSize: 13, fontWeight: '500', color: "#FFFFFF" }}>{`Role: #${item.role}`}</Text>
+                  <Text style={{ fontSize: 13, fontWeight: '500', color: "#FFFFFF" }}>Created At: {new Date(item.creationAt).toLocaleDateString()}</Text>
+                </View>
+              )}
+            </View>
+          )}
+        />
+      }
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     paddingHorizontal: 20,
-    marginTop: 40
+    marginTop: 40,
   },
-  mainContiner: {
-    padding: 12,
-    backgroundColor: "#eeeeee",
+  accordionHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 0.1,
+    borderRadius: 8,
     paddingHorizontal: 20,
-    borderRadius: 10,
-    borderWidth: 1,
-    marginBottom: 15
-  }
-})
+    paddingVertical: 10,
+    marginBottom: 10,
+    backgroundColor: '#eee'
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  userName: {
+    fontSize: 16,
+    width: '65%'
+  },
+  toggleIcon: {
+    fontSize: 40,
+    fontWeight: "900"
+  },
+  accordionContent: {
+    padding: 10,
+    backgroundColor: '#000',
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 5,
+    borderRadius: 5
+  },
+});
 
 export default App;
-
